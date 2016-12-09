e99fa4a6c1024f39385e7a589d10f25079d5550d
Pierre <pierre.bouillon@openmailbox.org>
2016-12-09 10:38:16 +0100

    Update Question1.sql

--- Question1.sql
+++ Question1.sql
@@ -6,13 +6,13 @@ create or replace function calculMoyenneAltitude() returns integer as
 		stationsParcourues cursor for select altitude from STATION;
 		altitudeTotale integer := 0;
 		stationsTotales integer := 0;
-    moyenneFinale integer :=0;
+    		moyenneFinale integer :=0;
 	begin
-		for cRecord in stationsParcourues loop
-			altitudeTotale = altitudeTotale+cRecord.altitude;
+		for altLue in stationsParcourues loop
+			altitudeTotale = altitudeTotale+altLue.altitude;
 			stationsTotales = stationsTotales+1 ;
 		end loop;
-    moyenneFinale :=altitudeTotale/stationsTotales;
-		return moyenneFinale
+   		moyenneFinale :=altitudeTotale/stationsTotales;
+		return moyenneFinale;
 	end;
 	$$ language plpgsql
