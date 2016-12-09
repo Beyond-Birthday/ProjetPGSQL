944754a7df3d602995edf71ea4bf3ff4b619c507
Pierre <pierre.bouillon@openmailbox.org>
2016-12-09 10:37:26 +0100

    Question 3 fonctionnelle

--- Question3.sql
+++ Question3.sql
@@ -1,1 +1,13 @@
-
+create or replace function capsPremiereLettreTousNoms() returns integer as 
+	$$
+	declare 
+		SkieursLus cursor for select nomSkieur from SKIEUR;
+		lignesModifiees integer := 0;
+	begin
+		for nomLus in SkieursLus loop
+			capsPremiereLettreNom(nomLus.nomSkieur);
+			lignesModifiees = lignesModifiees+1;
+		end loop;
+    return lignesModifiees;
+	end;
+	$$ language plpgsql
