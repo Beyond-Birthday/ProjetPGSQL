f6bd3ff253794ed1de1edafa1c6fa6eaaabb0b01
Jérémy Roussey <jeremyroussey@yahoo.fr>
2016-12-09 10:46:20 +0100

    Q2 fonctionnel

--- Question2.sql
+++ Question2.sql
@@ -1,1 +1,19 @@
+CREATE OR REPLACE FUNCTION capsPremiereLettreNom(name VARCHAR) RETURNS VARCHAR AS
+    $$
+    
+    DECLARE
 
+        len int := CHAR_LENGTH(name);
+        result varchar := '';
+
+    BEGIN
+        name = LOWER(name);
+        
+        result = CONCAT(UPPER(LEFT(name, 1)), LOWER(SUBSTRING(name, 2)));
+        
+        RETURN result;
+            
+    END;
+    
+    $$ language plpgsql;
+   
