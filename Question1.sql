1ce900e46201c1fbc0616b88488a33892c3e8ba3
Pierre <pierre.bouillon@openmailbox.org>
2016-12-09 10:31:01 +0100

    Question1.sql fonctionnelle

--- Question1.sql
+++ Question1.sql
@@ -1,1 +1,18 @@
+-- Requete 1
 
+create or replace function calculMoyenneAltitude() returns integer as 
+	$$
+	declare 
+		stationsParcourues cursor for select altitude from STATION;
+		altitudeTotale integer := 0;
+		stationsTotales integer := 0;
+    moyenneFinale integer :=0;
+	begin
+		for cRecord in stationsParcourues loop
+			altitudeTotale = altitudeTotale+cRecord.altitude;
+			stationsTotales = stationsTotales+1 ;
+		end loop;
+    moyenneFinale :=altitudeTotale/stationsTotales;
+		return moyenneFinale
+	end;
+	$$ language plpgsql
